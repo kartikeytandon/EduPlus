7aceeda7bed606b5b38bfa3af3fc3781b305104e
Kartikey Tandon <tandonkartiey11@gmail.com>
2023-11-01 23:59:12 +0530

    Project Setup - Project Launch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='my-16'>
+      <h1 className='text-5xl text-center'>Edu+</h1>
+      <h1 className='text-xl text-center my-2'>A New generation Product for for the Smarter World</h1>
+    </div>
+  )
+}
+
+export default App
